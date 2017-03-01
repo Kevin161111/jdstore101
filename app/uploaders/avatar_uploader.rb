5b226d9923dd3409913145ff29dc06dffa9dde72
Kevin161111 <hwha2008@hotmail.com>
2017-03-01 22:45:44 +0800

    change avator size

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -17,7 +17,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process resize_to_fill: [800, 800]
 
   version :thumb do
-    process resize_to_fill: [200,200]
+    process resize_to_fill: [270,240]
   end
 
   version :medium do
